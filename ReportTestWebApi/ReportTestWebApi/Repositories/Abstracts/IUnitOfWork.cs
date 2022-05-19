88297f9cc52b9fac4f1796ad91ccbce4e7e43985
Letjobana <suzanm@gerbertech.co.za>
2022-05-19 11:38:28 +0200

    Added Unit of Work

---
+++ ReportTestWebApi/ReportTestWebApi/Repositories/Abstracts/IUnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReportTestWebApi.Repositories.Abstracts
+{
+    public interface IUnitOfWork
+    {
+        IApiRepository ApiRepository { get; }
+    }
+}
---
+++ ReportTestWebApi/ReportTestWebApi/Repositories/Concretes/UnitOfWork.cs
@@ -0,0 +1,17 @@
+﻿using ReportTestWebApi.Repositories.Abstracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReportTestWebApi.Repositories.Concretes
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        public IApiRepository ApiRepository { get; private set; }
+        public UnitOfWork()
+        {
+            ApiRepository = new ApiRepository();
+        }
+    }
+}
