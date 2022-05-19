51da9f4e080b8ead417865873e71872fa857dd23
Letjobana <suzanm@gerbertech.co.za>
2022-05-19 10:43:07 +0200

    Added Credential Model

---
+++ ReportTestWebApi/ReportTestWebApi/Models/Credential.cs
@@ -0,0 +1,14 @@
+﻿using Geotab.Checkmate;
+
+namespace ReportTestWebApi.Models
+{
+    public class Credential
+    {
+        public string Username { get; set; }
+        public string Server { get; set; }
+        public string SessionId { get; set; }
+        public string Database { get; set; }
+        public string GroupFilter { get; set; }
+        public API API => new API(Username, null, SessionId, Database, Server);
+    }
+}
